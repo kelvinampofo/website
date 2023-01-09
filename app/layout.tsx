baaf8003939ead76ccf3b52b379efc5b4d3b8d35
Kelvin Ampofo <kelvinamp@outlook.com>
2023-01-09 22:47:25 +0000

    feat: add @next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
+import { Inter as SansFont } from '@next/font/google';
 import '../styles/globals.css';
 import { RootLayoutProps } from '../types/index';
 
+const sansFont = SansFont({
+  subsets: ['latin'],
+  variable: '--font-inter'
+});
+
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={sansFont.className}>
       <head />
       <body>{children}</body>
     </html>
