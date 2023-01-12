8cb0687d1f5d55dac3493255b2a2c6901af726eb
Kelvin Ampofo <kelvinamp@outlook.com>
2023-01-12 19:35:46 +0000

    feat[component]: add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter as SansFont } from '@next/font/google';
+import { Footer } from '../components/Footer';
 import '../styles/globals.css';
 import { RootLayoutProps } from '../types/index';
 
@@ -11,7 +12,10 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" className={sansFont.className}>
       <head />
-      <body>{children}</body>
+      <body className="mx-auto flex min-h-screen max-w-3xl flex-col dark:bg-[#101010] dark:text-gray-300">
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="absolute bottom-0 mx-auto mb-3 flex w-full max-w-3xl items-center justify-center md:text-base">
+      © {new Date().getFullYear()} Kelvin Ampofo. All Rights Reserved.
+    </footer>
+  );
+}
